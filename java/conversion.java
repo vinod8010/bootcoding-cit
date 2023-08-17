f96e1c4f8221cec4bd6752c6021b9127f9d04ace
vinod8010 <vinodballa36@gmail.com>
2023-08-17 15:51:06 +0530

    vinod java program

---
+++ java/conversion.java
@@ -0,0 +1,12 @@
+public class conversion {
+    public static void main(String[] args) {
+        int hour = 1;
+        int minutes =3;
+        int seconds = minutes * 60;
+        int hourseconds = hour*60*60;
+
+
+        System.out.println("hour to seconds :" + hourseconds);
+        System.out.println("minutes to seconds:" + seconds);
+    }
+}
---
+++ java/incriment.java
@@ -0,0 +1,7 @@
+public class incriment {
+    public static void main(String[] args) {
+        int a=10;
+        int b= a++ + 10;
+        System.out.println(b);
+    }
+}
